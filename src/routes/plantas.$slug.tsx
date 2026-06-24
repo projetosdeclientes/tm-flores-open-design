44b4dfd37f81b5b78a19fae98460f8cdb8018848
Luigi <lavandaportaguardanapos@gmail.com>
2026-06-24 16:01:52 -0300

    refactor: substitui React/TanStack por site estático com HTML direto do Lovable
    - Remove todo o código-fonte React/TanStack (src/, dist/, node_modules/)
    - Remove configs de build (vite, tsconfig, eslint, prettier, bun, Netlify SSR)
    - Move os assets do site (HTML, CSS, JS, imagens) de public/ para a raiz
    - Mantém gitignore limpo para site estático
    - Site copiado tag por tag do ZIP de referência do Lovable

--- src/routes/plantas.$slug.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { ProductPage } from '@/pages/ProductPage'
-
-export const Route = createFileRoute('/plantas/$slug')({
-  component: ProductPage,
-})
