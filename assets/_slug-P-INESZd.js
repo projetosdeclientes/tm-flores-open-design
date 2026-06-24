614b910204e494c459a6277f845acfdc966fe656
Luigi <lavandaportaguardanapos@gmail.com>
2026-06-24 18:41:34 -0300

    Revert "fix: bfcache reveal + performance (orbs, imagens lazy)"
    This reverts commit c3e5fe623af819a611b3f6749f03aec85cf7973b, reversing
    changes made to 23e6c8edc018542b8f38a078bc9b9d2bb30b20e2.

--- assets/_slug-P-INESZd.js
+++
@@ -1,1 +0,0 @@
-import{P as o}from"./ProductPage-BrrkDKos.js";import"./index-B5w3pvWp.js";import"./Navbar-CUoTOUAZ.js";import"./Footer-BJN8qnZF.js";import"./ProductCard-BfK_MHcT.js";const n=o;export{n as component};
